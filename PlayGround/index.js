499336919e78eb88fa50f12c34a47b4a9f0a97b7
Ayanfe <peleyeju_femi@yahoo.com>
2020-04-27 19:50:22 +0300

    commit

---
+++ PlayGround/index.js
@@ -0,0 +1,18 @@
+// write hello world Program 
+// print out hello world to the screen or consore
+//print out a greeting with a name
+// search how to print out to consore in javascript
+
+//Begin
+//accept a name 
+// print out greeting 
+//end
+
+var name = "Mr Aynfe";
+var greeting = "Hello" + name;
+console.log(greeting);
+
+
+
+
+
