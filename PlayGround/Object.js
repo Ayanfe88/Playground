b627dfd13563597cae8ed8b3c30a5243a69b2f45
Ayanfe <peleyeju_femi@yahoo.com>
2020-04-30 02:51:25 +0300

    HomeWork

--- PlayGround/Object.js
+++ PlayGround/Object.js
@@ -1,15 +1,26 @@
-// Task: creating object of a person
+// Task1: creating object of a person
 // name, age, sex
-// create a function that returns the year of birth
+// Task2: create a function that returns the year of birth
 
 var Person = {
     Name : " Femi ",
-    Age :  24, 
-    Sex : " Male "};
-//console.log(Person);
-var year = 2020 - Age; // calculating the year of birth
-function getBirthYear() 
+    Age : 28 , 
+    Sex : " Male ",
+};
+//console.log( " Your name is " +Person.Name);
+ var Year = new Date().getFullYear() - Person.Age;  // calculating the year of birth
+function getBirthYear() //function that returns the year of birth
 {
- return year;
-}
-console.log(getBirthYear(year));
+   console.log(" Year of birth is " + Year);}
+
+getBirthYear();
+ 
+// Homework
+function personClassifier()
+{ if (new Date().getFullYear() <= Year)
+    {
+    console.log(" Millennial ");}
+else{
+    console.log( " Not Millennial");
+}}
+personClassifier();
